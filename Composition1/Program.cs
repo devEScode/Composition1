78b5948334e4d76d02357d9e1e5f8d0aea95d0f9
Edson Santos <dev.escode@gmail.com>
2020-06-15 11:37:18 -0300

    Criada as classes Worker, Department, HourContract e enum WorkLevel

---
+++ Composition1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Composition1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
